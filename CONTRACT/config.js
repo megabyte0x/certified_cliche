457f46d3f560d6515003560c5a2eb19055c49def
Yash Garg <yashygkf@gmail.com>
2022-01-31 00:23:00 +0530

    ready for submission

--- CONTRACT/config.js
+++ CONTRACT/config.js
@@ -1,5 +1,5 @@
-export const nftAddress = "0x7Cd0f43839B238e6bd96f19Ee7734190bbaFBa01";
+export const nftAddress = "0xc2b0a775dade1AA50713875951fD6ee941c5885E";
 
-export const nftTransferAddress = "0xA2AEA2b698e28C1ce682693b3397C89F77A6379D";
+export const nftTransferAddress = "0xeA4BbAb1bBbC9b00D321Cb5fDa7A24e89971935a";
 
 export const rpc_url = `https://rpc-mumbai.maticvigil.com/v1/3563bdf92c4890518396bd186677a6bc37e727f3`;
