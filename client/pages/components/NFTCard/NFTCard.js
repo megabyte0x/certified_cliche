e13addb4cdadc6e4082d2c73300c3ed89e67d1d5
Rajdeep <rajdipgupta019@gmail>
2022-03-03 12:27:38 +0530

    NFT Card Added on pages

--- client/pages/components/NFTCard/NFTCard.js
+++ client/pages/components/NFTCard/NFTCard.js
@@ -1,17 +1,18 @@
 import React from "react";
 
-const NFTCard = () => {
+const NFTCard = ({key, nftImage, nftName, nftDesciption}) => {
   return (
-    <div>
+    <div key={key}>
       <div className=" w-[261px] h-[318px] border-[1px] rounded-3xl border-[#C5C5C5] mx-20">
         <img
-          src="https://static01.nyt.com/images/2021/03/12/arts/11nft-auction-cryptopunks-print/11nft-auction-cryptopunks-print-mobileMasterAt3x.jpg"
+          src={nftImage}
+          alt={nftName}
           className="rounded-t-3xl w-[261px] h-[246px]"
         />
         <div className="py-2 px-6">
-          <p className="text-lg font-semibold">Dummy name </p>
+          <p className="text-lg font-semibold">{nftName} </p>
           <p className="text-sm mt-1">
-            Lorem ipsum dolor sit.{" "}
+            {nftDesciption}.{" "}
           </p>
         </div>
       </div>
--- client/pages/index.js
+++ client/pages/index.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import Web3Modal from "web3modal";
 import Popup from 'reactjs-popup';
 import classes from '../styles/Home.module.css'
+import NFTCard from "./components/NFTCard/NFTCard";
 
 
 import { nftAddress, nftTransferAddress, rpc_url } from "../../CONTRACT/config";
@@ -32,7 +33,7 @@ export default function Home() {
     // const provider = new ethers.providers.JsonRpcProvider(rpc_url);
 
     // For the LocalHost
-    // const provider = new ethers.providers.JsonRpcProvider();
+    const provider = new ethers.providers.JsonRpcProvider();
 
     const tokenContract = new ethers.Contract(nftAddress, NFT.abi, provider);
     const marketContract = new ethers.Contract(nftTransferAddress, NFTTransfer.abi, provider);
@@ -92,31 +93,15 @@ export default function Home() {
       <div className="px-4" style={{ maxWidth: '1600px' }}>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4 " >
           {
-            nfts.map((nft, i) => (
-              <div key={i} className="border shadow rounded-xl overflow-hidden">
-                <img style={{ objectFit: "cover", height: 300 }} src={nft.image} height={300} width={300} alt={nft.name} />
-                <div className="p-4">
-                  <p className="text-2xl font-semibold">{nft.name}</p>
-                  <div style={{ height: '70px', overflow: 'hidden' }}>
-                    <p className="text-gray-400">{nft.description}</p>
-                  </div>
-                </div>
-                {transferSelected == i && <div className={`pb-2 pt-2 bg-black ${classes.tranfer_text_block}`}>
-                  <input
-                    className={`${classes.tranfer_text} mt-2 mb-2 ml-4 mr-4 border rounded`}
-                    placeholder="Applicant Wallet Address"
-                    onChange={e => setWalletAdd(e.target.value)}
-                  />
-                  <button onClick={() => transferNFT(nft)} className={classes.transfer_main_btn}>Done</button>
-                </div>}
-                {
-                  transferSelected != i &&
-                  <div className="p-4 bg-black">
-                    <button className="w-full bg-pink-500 text-white font-bold py-2 px-12 rounded" onClick={() => { setTransferSelected(i) }}>TRANSFER </button>
-                  </div>
-                }
-              </div>
-            ))
+            nfts.map((nft, i) => {
+              return(
+              <NFTCard 
+                key={i}
+                nftImage={nft.image}
+                nftName={nft.name}
+                nftDesciption={nft.description}
+              />
+            )})
           }
         </div>
       </div>
