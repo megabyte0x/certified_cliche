5efa81a9392ac24c7ce677adabd47eea7931d3f0
Rajdeep <rajdipgupta019@gmail>
2022-03-04 23:28:26 +0530

    Event Card fixed

--- client/pages/components/EventCard/EventCard.js
+++ client/pages/components/EventCard/EventCard.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 const EventCard = ({eventImage, eventName, eventDesciption, key}) => {
   return (
-    <div key={key}>
-        <div className=" w-[435px] h-[255px] rounded-3xl relative  z-50">
+    <div key={key} className="w-full">
+        <div className="h-[255px] rounded-3xl relative z-50">
         <img
           src={eventImage}
           alt={eventName}
-          className="rounded-3xl object-cover w-[435px] h-[255px] absolute"
+          className="rounded-3xl object-cover h-[255px] absolute w-full"
           />
           {/* This is for the shadow on the Image */}
-        <div className=" relative z-20 bg-gradient-to-t from-black to-white w-[435px] h-[255px] opacity-50 rounded-3xl"></div> 
+        <div className="w-full relative z-20 bg-gradient-to-t from-black to-white h-[255px] opacity-50 rounded-3xl"></div> 
         {/* Text part in the Image */}
         <div className=' absolute bottom-7 left-10 text-white z-30'>
           <p className="text-2xl font-semibold">{eventName} </p>
