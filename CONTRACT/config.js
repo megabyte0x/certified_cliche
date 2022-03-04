410160200d55897cca68d7c8d9a35dff57413cda
Supriya Prakash <supriyaprakash2229@gmail.com>
2022-03-04 18:10:57 +0530

    initial commit

--- CONTRACT/config.js
+++ CONTRACT/config.js
@@ -1,5 +1,5 @@
-export const nftAddress = "Paste it here";
+export const nftAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
 
-export const nftTransferAddress = "Paste it here";
+export const nftTransferAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
 
-export const rpc_url = `Paste it here`;
+export const rpc_url = "https://rpc-mumbai.maticvigil.com/v1/d62fa92f48381dff323762c8dda97b687bb602e5";
