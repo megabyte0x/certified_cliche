47a1239df851752ee4ea664d02a99083e2cee565
Supriya Prakash <supriyaprakash2229@gmail.com>
2022-03-13 12:20:34 +0530

    changed config in Contract

--- CONTRACT/config.js
+++ CONTRACT/config.js
@@ -1,5 +1,5 @@
-export const nftAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const nftAddress = "Paste it here";
 
-export const nftTransferAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const nftTransferAddress = "Paste it here";
 
 export const rpc_url = `Paste it here`;
--- client/config.js
+++ client/config.js
@@ -1,2 +1,4 @@
-export const ServerUrl="Paste your moralis server url here"
-export const AppId="Paste your Moralis AppID here"
+// export const ServerUrl="Paste your moralis server url here"
+// export const AppId="Paste your Moralis AppID here"
+export const ServerUrl="https://w33fflrcjbyj.usemoralis.com:2053/server"
+export const AppId="McqvaCWqrjIcT5HRJbFAS4EoyvAXV9y5eH2pywyS"
