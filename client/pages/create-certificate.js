88a715d8e1e50e0c459f63f72eff84592baa72a4
Abhinav0909 <abhinavkarforma2@gmail.com>
2022-03-27 21:57:10 +0530

    🐛 Fixed all bugs

--- client/pages/create-certificate.js
+++ client/pages/create-certificate.js
@@ -115,10 +115,10 @@ export default function CreateItem() {
     setDropdown(!dropdown);
   }
   return (
-    <div className="grid place-items-center  ">
+    <div className="grid place-items-center ">
       <div className="lg:w-1/2 flex flex-col pb-12 w-5/6 md:4/6">
         <div className="md:grid md:grid-cols-3 ">
-          <div className="flex flex-col col-span-2 md:mr-10">
+          <div className="flex flex-col col-span-2 md:mr-12">
             <input
               placeholder="Name"
               className="mt-8 border rounded-2xl p-4 mb-4 text-[#010101]"
@@ -135,19 +135,28 @@ export default function CreateItem() {
             />
           </div>
 
-          <div className="mt-6 hidden md:block ">
-            <div className=" flex relative z-10 text-white text-5xl cursor-pointer">
-              <p className="transform rotate-90 absolute top-24 left-24">
-                &#x270E;
-              </p>
-              <p className="absolute top-32 left-24"> &#9473;</p>
-            </div>
-            <Image
-              src="/assests/img.jpg"
-              width={385}
-              height={385}
-              alt="Alt image"
-            />
+          <div className="md:block cursor-pointer relative  ">
+                <div className="md:w-[270px] md:h-[276px] w-[200px] h-[200px] relative mx-auto mt-10 mb-4 ">
+                  <Image
+                    src="/assests/img.jpg"
+                    layout="fill"
+                    alt="Alt image"
+                  />
+                </div>
+                <div className="absolute  md:top-[19vw] md:right-[9vw] xl:top-[46%] lg:left-[41%] lg:top-[20%] top-[36%] right-[45%]   ">
+                  <svg
+                    width="46"
+                    height="46"
+                    viewBox="0 0 76 76"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M72.5 68.375H3.5C1.84062 68.375 0.5 69.7156 0.5 71.375V74.75C0.5 75.1625 0.8375 75.5 1.25 75.5H74.75C75.1625 75.5 75.5 75.1625 75.5 74.75V71.375C75.5 69.7156 74.1594 68.375 72.5 68.375ZM14.1594 60.5C14.3469 60.5 14.5344 60.4813 14.7219 60.4531L30.4906 57.6875C30.6781 57.65 30.8563 57.5656 30.9875 57.425L70.7281 17.6844C70.815 17.5976 70.884 17.4946 70.931 17.3812C70.9781 17.2678 71.0023 17.1462 71.0023 17.0234C71.0023 16.9007 70.9781 16.7791 70.931 16.6657C70.884 16.5523 70.815 16.4492 70.7281 16.3625L55.1469 0.771875C54.9688 0.59375 54.7344 0.5 54.4812 0.5C54.2281 0.5 53.9938 0.59375 53.8156 0.771875L14.075 40.5125C13.9344 40.6531 13.85 40.8219 13.8125 41.0094L11.0469 56.7781C10.9557 57.2804 10.9883 57.7972 11.1418 58.284C11.2954 58.7708 11.5653 59.2129 11.9281 59.5719C12.5469 60.1719 13.325 60.5 14.1594 60.5V60.5Z"
+                      fill="white"
+                    />
+                  </svg>
+          </div>
           </div>
         </div>
         <div className="flex my-7 md:space-x-5 space-x-2">
@@ -162,59 +171,70 @@ export default function CreateItem() {
         </div>
         {checkBox === true && (
           <>
-            <input
-              placeholder="Event Name"
-              className="mt-2 border rounded-2xl p-4 mb-4 text-[#010101] md:w-3/5"
-            />
-            <p className="text-[#7B7B7B] ">or create new event/group</p>
+            <div className=" relative md:max-w-[29rem]">
+              <p
+                className="mt-2 border rounded-2xl p-4 opacity-60 mb-4 text-[#010101]"
+                onClick={ShowDropdown}
+              >
+                Select the event
+              </p>
+              {dropdown ? (
+                <p
+                  className="absolute right-6 top-1/3 text-xs cursor-pointer  text-[#6C6C6C] transform rotate-180"
+                  onClick={ShowDropdown}
+                >
+                  &#9660;
+                </p>
+              ) : (
+                <p
+                  className="absolute right-6 top-[37%] text-xs cursor-pointer  text-[#6C6C6C]  "
+                  onClick={ShowDropdown}
+                >
+                  &#9660;
+                </p>
+              )}
+              {dropdown && (
+                <l1 className=" absolute top-16 left-2  shadow-2xl bg-[#DEE4E7] w-full text-lg text-[#010101] leading-7 opacity-60 text-center py-1 font-normal z-10 cursor-pointer">
+                  GirlScript Summer of Code
+                </l1>
+              )}
+            </div>
+            <p className="text-[#7B7B7B] md:mb-2 md:mt-1 md:ml-2">
+              or create new event/group
+            </p>
             <div className="md:grid md:grid-cols-3 ">
-              <div className="flex flex-col col-span-2 md:mr-10">
-                <div className=" relative">
-                  <p
-                    className="mt-2 border rounded-2xl p-4 opacity-60 mb-4 text-[#010101]"
-                    onClick={ShowDropdown}
-                  >
-                    Select the event
-                  </p>
-                  {dropdown ? (
-                    <p
-                      className="absolute right-6 top-1/3 text-xs cursor-pointer  text-[#6C6C6C] transform rotate-180"
-                      onClick={ShowDropdown}
-                    >
-                      &#9660;
-                    </p>
-                  ) : (
-                    <p
-                      className="absolute right-6 top-[37%] text-xs cursor-pointer  text-[#6C6C6C]  "
-                      onClick={ShowDropdown}
-                    >
-                      &#9660;
-                    </p>
-                  )}
-                  {dropdown && (
-                    <l1 className=" absolute top-16 left-2  shadow-2xl bg-[#DEE4E7] w-full text-lg text-[#010101] leading-7 opacity-60 text-center py-1 font-normal z-10 cursor-pointer">
-                      GirlScript Summer of Code
-                    </l1>
-                  )}
-                </div>
+              <div className="flex flex-col col-span-2 md:mr-12">
+                <input
+                  placeholder="Event Name"
+                  className="mt-2 border rounded-2xl p-4 mb-4 text-[#010101] "
+                />
                 <textarea
                   placeholder="Event Description"
                   className="mt-3 mb-8 border rounded-2xl p-4 text-[#010101] md:h-40 md:min-h-fit"
                 />
               </div>
-              <div className="hidden md:block">
-                <div className=" bg-red-800 relative z-10 text-white text-5xl cursor-pointer">
-                  <p className="absolute top-24 left-24 z-10 text-white text-5xl transform rotate-90  ">
-                    &#x270E;
-                  </p>
-                  <p className="absolute top-32 left-24"> &#9473;</p>
+              <div className="md:block cursor-pointer relative  ">
+                <div className="md:w-[270px] md:h-[276px] w-[200px] h-[200px] relative mx-auto mb-10">
+                  <Image
+                    src="/assests/img2.jpg"
+                    layout="fill"
+                    alt="Alt image"
+                  />
                 </div>
-                <Image
-                  src="/assests/img2.jpg"
-                  width={385}
-                  height={385}
-                  alt="Alt image"
-                />
+                <div className="absolute  md:bottom-[17vw] md:right-[9vw] xl:top-[38%] lg:left-[40%] lg:top-[20%] bottom-[35%] right-[45%]  ">
+                  <svg
+                    width="46"
+                    height="46"
+                    viewBox="0 0 76 76"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M72.5 68.375H3.5C1.84062 68.375 0.5 69.7156 0.5 71.375V74.75C0.5 75.1625 0.8375 75.5 1.25 75.5H74.75C75.1625 75.5 75.5 75.1625 75.5 74.75V71.375C75.5 69.7156 74.1594 68.375 72.5 68.375ZM14.1594 60.5C14.3469 60.5 14.5344 60.4813 14.7219 60.4531L30.4906 57.6875C30.6781 57.65 30.8563 57.5656 30.9875 57.425L70.7281 17.6844C70.815 17.5976 70.884 17.4946 70.931 17.3812C70.9781 17.2678 71.0023 17.1462 71.0023 17.0234C71.0023 16.9007 70.9781 16.7791 70.931 16.6657C70.884 16.5523 70.815 16.4492 70.7281 16.3625L55.1469 0.771875C54.9688 0.59375 54.7344 0.5 54.4812 0.5C54.2281 0.5 53.9938 0.59375 53.8156 0.771875L14.075 40.5125C13.9344 40.6531 13.85 40.8219 13.8125 41.0094L11.0469 56.7781C10.9557 57.2804 10.9883 57.7972 11.1418 58.284C11.2954 58.7708 11.5653 59.2129 11.9281 59.5719C12.5469 60.1719 13.325 60.5 14.1594 60.5V60.5Z"
+                      fill="white"
+                    />
+                  </svg>
+                </div>
               </div>
             </div>
           </>
@@ -225,7 +245,14 @@ export default function CreateItem() {
           className="my-4 hidden"
           onChange={onChange}
         />
-        {fileUrl && <img className="rounded mt-4" width="350" src={fileUrl} alt="fileLoading"/>}
+        {fileUrl && (
+          <img
+            className="rounded mt-4"
+            width="350"
+            src={fileUrl}
+            alt="fileLoading"
+          />
+        )}
         <button
           onClick={createCertificate}
           className="font-bold  bg-[#4A3CED] text-white py-5 text-xs  text-center shadow-lg md:w-[18.5%] rounded-lg   "
