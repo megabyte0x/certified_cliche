c96a42157d9f4c53811d7c76ea5d67402f48af19
SaachiKaup <saachi.kaup@gmail.com>
2022-06-02 14:36:36 +0530

    Added received certificates page with NFTCard Component.

--- client/pages/received-certificates.js
+++ client/pages/received-certificates.js
@@ -4,8 +4,10 @@ import woman from "../public/woman.svg";
 import amazon from "../public/amazon.svg";
 import puppy from "../public/puppy.svg";
 import metaverse from "../public/metaverse.svg";
+import NFTCard from "./components/NFTCard/NFTCard";
+import "../styles/received-certificates.module.css";
 
-const HomeComponent = () => {
+const ReceivedCertificates = () => {
   const certificateData = [
     {
       id: 1,
@@ -57,41 +59,94 @@ const HomeComponent = () => {
     },
   ];
   return (
-    <div className="m-4 md:m-6 lg:m-8 ">
+  <div className="m-4 md:m-6 lg:m-8">
+      <h3 className="md:text-[1.3rem] text-[1.2rem] font-semibold ml-3 md:ml-5 lg:ml-7">
+        Received Certificates
+      </h3>
+      <div className="flex justify-center items-center">
+        <div className="w-[95%]  bg-[#bbbaba] h-[1px] mb-10 mt-3 "></div>
+      </div>
+      <div className="flex justify-center">
+        <div className="p-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-16 pt-4">
+            {certificateData.map((certificate, i) => {
+              return (
+                <NFTCard
+                  key={i}
+                  nftImage={certificate.img}
+                  nftName={certificate.name}
+                  nftDesciption={certificate.para}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReceivedCertificates;
+
+/*To remove
+<div className="m-4 md:m-6 lg:m-8">
       <h3 className="md:text-[1.3rem] text-[1.2rem] font-semibold ml-3 md:ml-5 lg:ml-7">
         My Certificates
       </h3>
-
+      
       <div className="flex justify-center items-center">
         <div className="w-[95%]  bg-[#bbbaba] h-[1px] mb-10 mt-3 "></div>
       </div>
       <div className="flex flex-wrap items-center md:justify-start justify-center">
         {certificateData.map((item) => {
           const { id } = item;
-
+          //console.log('item: ', item.id)
+          //console.log('id:', id)
           return (
-            <div key={id}>
-              <HomeCard {...item} />
-            </div>
+          <div>
+          {/<div key={id} className = "received-certificates">
+          <HomeCard {...item} /> 
+
+            <NFTCard 
+                  id = {item.id}
+                  nftImage = {item.img}
+                  nftName = {item.heading}
+                  nftDesciption = {item.para}
+          /> /}
+          </div>
           );
         })}
       </div>
-    </div>
-  );
-};
+      {/<div className="flex justify-center items-center">
+        <div className="px-4" style={{ maxWidth: '1600px' }}>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 px-4 gap-4 justify-center" >/}
 
-export default HomeComponent;
+      <div >
+        <div>
+          <div className = "grid lg:grid-cols-3 px-4 gap-4" 
+            style = {{paddingLeft: '0px'}}>
+              {certificateData.map((item) => {
+                const { id } = item;
+                console.log('item: ', item.id)
+                console.log('id:', id)
+                  return (
+                  <div>
+                    {*<div key={id} className = "received-certificates">
+                    <HomeCard {...item} />*}
 
-const HomeCard = ({ img, heading, para }) => {
-  return (
-    <div className="w-[210px] h-[254px] m-6 md:m-7 lg:m-8 rounded-[10%] border-[1px] border-[transparent] border-solid bg-[#f1f1f1]">
-      <div>
-        <img src={img} alt="" className="w-[284px] h-[196px] " />
-      </div>
-      <div className="flex-col justify-center flex items-start ">
-        <h5 className="font-semibold my-1 ml-4 text-[14px]">{heading}</h5>
-        <p className="ml-4 -mt-1 text-[12px]">{para}</p>
-      </div>
-    </div>
-  );
-};
+                      <NFTCard 
+                            id = {item.id}
+                            nftImage = {item.img}
+                            nftName = {item.heading}
+                            nftDesciption = {item.para}
+                        /> 
+                  </div>
+                  );
+                }  
+                )
+              }
+          </div>
+        </div>
+          </div> 
+    </div> 
+*/
